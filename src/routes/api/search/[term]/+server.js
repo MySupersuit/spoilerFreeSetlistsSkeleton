1c09f6a49b39453ac0ca28bdb5f84c80aa25a07e
Tom Moran <32645063+MySupersuit@users.noreply.github.com>
2023-07-08 18:39:53 +0100

    add logs

--- src/routes/api/search/[term]/+server.js
+++ src/routes/api/search/[term]/+server.js
@@ -4,6 +4,9 @@ export async function GET({ params }) {
 	const { term } = params;
 	let url = `https://api.setlist.fm/rest/1.0/search/artists?sort=relevance&artistName=${term}`;
 
+  console.log('url', url);
+	console.log('api', API_KEY);
+
 	const response = await fetch(url, {
 		headers: {
 			Accept: 'application/json',
@@ -11,6 +14,7 @@ export async function GET({ params }) {
 			'Accept-encoding': 'gzip, deflate, br'
 		}
 	});
+  console.log('resp', response);
 	if (response.status !== 200) {
 		return new Response(JSON.stringify({ status: response.status }));
 	}
